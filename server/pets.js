d8e650bb2ac92855d3509b848e04e17a453827f4
Andres Carrasco <andrescm1998@outlook.com>
2022-10-04 14:51:46 +0100

    Add url for image to cat object

--- server/pets.js
+++ server/pets.js
@@ -4,7 +4,8 @@ const pets  = [{
     age: 2,
     color: "white",
     favFood: "Tuna",
-    breed: "Ragdol"
+    breed: "Abyssinian",
+    url: "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg"
 
 },{
     name: "Garfield",
@@ -12,7 +13,8 @@ const pets  = [{
     age: 5,
     color: "golden",
     favFood: "Lasagna",
-    breed: "Persian Tabby"
+    breed: "Persian Tabby",
+    url: "https://cdn2.thecatapi.com/images/3Pem6K30P.jpg"
 }]
 
 
